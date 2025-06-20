68157ddc1bbad30f4bc99d21ed5f4dd42afeb5fc
Atheria <diaviekone13@gmail.com>
2025-06-20 08:58:33 +0700

    Functions

--- src/commands/log.c
+++ src/commands/log.c
@@ -2,6 +2,5 @@
 #include <stdio.h>
 int cmd_log(int argc, char* argv[]) {
     printf("logging\n");
-    //TODO: IMPLEMENT
     return 0;
 }
--- src/core/hash.c
+++ src/core/hash.c
@@ -1,5 +1,1 @@
-//
-// Created by Atheria on 6/20/25.
-//
-
 #include "hash.h"
